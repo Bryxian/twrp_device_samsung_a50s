4066e2631f2be58c0f77d4eec8a1677f85812ad4
Bryxian <jonmielalexeypilartrinidad@gmail.com>
2026-06-24 14:52:53 +0800

    Rebrand device tree to A50s and switch to A50s kernel
    * Rebrand TWRP device tree from A50 to A50s
    * Update kernel configuration to use the A50s kernel
    * Adjust device-specific settings for A50s compatibility

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifneq ($(filter a50,$(TARGET_DEVICE)),)
+ifneq ($(filter a50sxx,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a50.mk
+    $(LOCAL_DIR)/twrp_a50sxx.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_a50-eng
